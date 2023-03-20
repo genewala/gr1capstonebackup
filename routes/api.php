ceab7155eb73f6faafc9fb36f92fb6a237c16abb
unknown <demoneyesrhada@gmail.com>
2023-03-20 20:14:52 +0800

    database working

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,9 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ConnectController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('connection', [ConnectController::class, 'store' ]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -23,4 +15,9 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout']);
 
-?>
+Route::get('/test', function () {
+    return "Hello";
+});
+
+// Route::post('store', "AuthApi@store");
+
